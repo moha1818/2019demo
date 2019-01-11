7b6f445c4a3c5fb13ddf4209050a7b4fc67f5ab5
moha <799845210@qq.com>
2019-01-11 17:30:58 +0800

    @ApiParam

--- src/main/java/com/sunland/controller/BussinessController.java
+++ src/main/java/com/sunland/controller/BussinessController.java
@@ -2,6 +2,7 @@ package com.sunland.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,7 +13,7 @@ public class BussinessController {
     @ResponseBody
     @RequestMapping(value = "/getlist",method = RequestMethod.GET)
     @ApiOperation(value = "查询订单明细",notes = "[@moha]")
-    public String selectBusiness(@RequestParam(value = "1-路停 2-停车场") int type, @RequestParam(value = "微信-token 安卓IOS-mobilenum") String token_id, @RequestParam(value = "客户端")String from){
+    public String selectBusiness(@ApiParam(value = "1-路停 2-停车场")@RequestParam int type, @ApiParam(value = "微信-token 安卓IOS-mobilenum")@RequestParam String token_id, @ApiParam(value = "客户端")@RequestParam String from){
 
         return "";
     }
